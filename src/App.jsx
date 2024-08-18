a5fe93e623df0b7cdac3f40a7b126d597c49857b
Square <aayushk4980@gmail.com>
2024-08-18 14:58:40 +0530

    Getting user on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import conf from './app/conf/conf.js';
+import { useDispatch } from 'react-redux';
+import authService from './app/services/authService.js';
+import { login, logout } from './app/store/features/authSlice.js';
+import { Footer, Header } from './components/index.js';
 
 export default function App() {
 
-  console.log(conf);
+  // Loading state.
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getUser()
+    .then(userData => {
+      if (userData) {
+        dispatch(login(userData));
+      } else {
+        console.log("Logout")
+        dispatch(logout());
+      }
+    })
+    .finally(() => {
+      console.log("Finally")
+      setLoading(false)})
+  }, []);
+
+  return !loading ? (
+    <div
+    className="min-h-screen flex flex-wrap content-between bg-gray-900 text-white"
+    >
+      Hi
+    </div>
+  ) : null;
 
   return (
     <>
     <h1 className="text-center m-10 text-3xl">Learning BAAS</h1>
+    <Header />
+    <Footer />
     </>
   );
 };
