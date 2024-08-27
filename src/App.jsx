4efc3991828a6dca0e6b022f22f147468135e54a
Square <aayushk4980@gmail.com>
2024-08-27 21:20:09 +0530

    Applied routing to Login, Home, and sign up pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import authService from './app/services/authService.js';
 import { login, logout } from './app/store/features/authSlice.js';
-import { Footer, Header } from './components/index.js';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { AuthLayout, Footer, Header } from './components/index.js';
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
 
 export default function App() {
 
@@ -24,11 +28,39 @@ export default function App() {
 
   return !loading ? (
     <>
-    <div className="bg-[#020187] text-white min-h-screen w-screen overflow-x-hidden overflow-y-auto">
-      <Header />
-      <Footer />
+      <div className="bg-[#020187] text-white min-h-screen w-screen overflow-x-hidden overflow-y-auto">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path='/' element={
+              <AuthLayout>
+                <HomePage />
+              </AuthLayout>
+            } />
 
-    </div>
+            <Route path='/home' element={
+              <AuthLayout>
+                <HomePage />
+              </AuthLayout>
+            } />
+
+            <Route path='/login' element={
+              <AuthLayout authentication={false}>
+                <LoginPage />
+              </AuthLayout>
+            } />
+
+            <Route path='/signup' element={
+              <AuthLayout authentication={false}>
+                <SignUpPage />
+              </AuthLayout>
+            } />
+
+          </Routes>
+        </Router>
+        <Footer />
+
+      </div>
     </>
   ) : (
     <div className="text-center">Loading...</div>
