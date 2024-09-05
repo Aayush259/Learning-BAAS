0777715c233a96bae90eb65ac7b2279892651485
Square <aayushk4980@gmail.com>
2024-09-05 11:53:52 +0530

    Moved router configuration from App.jsx to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import authService from './app/services/authService.js';
 import { login, logout } from './app/store/features/authSlice.js';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { AuthLayout, Footer, Header, Loader } from './components/index.js';
-import HomePage from './pages/HomePage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import SignUpPage from './pages/SignUpPage.jsx';
-import LandingPage from './pages/LandingPage.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
+import { Footer, Header, Loader } from './components/index.js';
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
 
@@ -31,45 +26,10 @@ export default function App() {
   return !loading ? (
     <>
       <div className="bg-black text-white min-h-screen w-screen overflow-x-hidden overflow-y-auto">
-        <Router>
-          <Header />
-          <Routes>
-            <Route path='/' element={
-              <AuthLayout authentication={false}>
-                <LandingPage />
-              </AuthLayout>
-            }
-            />
+        <Header />
 
-            <Route path='/home' element={
-              <AuthLayout authentication={true}>
-                <HomePage />
-              </AuthLayout>
-            }
-            />
+        <Outlet />
 
-            <Route path='/login' element={
-              <AuthLayout authentication={false}>
-                <LoginPage />
-              </AuthLayout>
-            }
-            />
-
-            <Route path='/signup' element={
-              <AuthLayout authentication={false}>
-                <SignUpPage />
-              </AuthLayout>
-            }
-            />
-
-            <Route path='/profile' element={
-              <AuthLayout authentication={true}>
-                <ProfilePage />
-              </AuthLayout>
-            } />
-
-          </Routes>
-        </Router>
         {/* <Footer /> */}
 
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,60 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './app/store/store.js';
 import { BackgroundBeamsWithCollision } from './components/ui/BackgroundBeamsWithCollision.jsx';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { AuthLayout, Loader } from './components/index.js';
+import LandingPage from './pages/LandingPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+
+      <Route path='/' element={
+        <AuthLayout authentication={false}>
+          <LandingPage />
+        </AuthLayout>
+      } />
+
+      <Route path='/home' element={
+        <AuthLayout authentication={true}>
+          <HomePage />
+        </AuthLayout>
+      }
+      />
+
+      <Route path='/login' element={
+        <AuthLayout authentication={false}>
+          <LoginPage />
+        </AuthLayout>
+      }
+      />
+
+      <Route path='/signup' element={
+        <AuthLayout authentication={false}>
+          <SignUpPage />
+        </AuthLayout>
+      }
+      />
+
+      <Route path='/profile' element={
+        <AuthLayout authentication={true}>
+          <ProfilePage />
+        </AuthLayout>
+      } />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <BackgroundBeamsWithCollision>
-        <App />
-      </BackgroundBeamsWithCollision>
-    </Provider>
+  <Provider store={store}>
+    <BackgroundBeamsWithCollision>
+    <RouterProvider router={router} />
+    </BackgroundBeamsWithCollision>
+  </Provider>
   </StrictMode>,
 );
